9f33dc113c58bf5c2554d487aa312603c7db4d59
ray <ray-debain>
2014-05-21 15:23:24 +0800

    add more learning for enum class

--- learn_cxx/learnEnumClass.cpp
+++ learn_cxx/learnEnumClass.cpp
@@ -11,14 +11,14 @@ enum class Side
 // http://stackoverflow.com/questions/2393439/can-i-overload-operators-on-enum-types-in-c
 //   Yes, operator overloading can be done on enum and class types. 
 //
-// http://stackoverflow.com/questions/12753543/is-it-possible-to-manually-define-a-conversion-for-an-enum-class
-//   No, it's not. Actually, an enum class is no class at all. 
-//   The class keyword is only used because suddenly changing the unscoped enum to a scoped enum would have mean reworking all enums codes. 
-
 bool operator !(Side s) {
    return s != Side::Null;
 }
 
+// http://stackoverflow.com/questions/12753543/is-it-possible-to-manually-define-a-conversion-for-an-enum-class
+//   No, it's not. Actually, an enum class is no class at all. 
+//   The class keyword is only used because suddenly changing the unscoped enum to a scoped enum would have mean reworking all enums codes. 
+//
 // // C++ syntax memo
 // class B
 // {
@@ -38,5 +38,11 @@ int main()
 
  using namespace std;
  cout << "opeator! (Side) = " << !s << endl;
+
+ if (!!s) // !! idiom of C++, to use any type in 'bool' place: First operator! to bool, then ! on bool 
+ {
+   cout << "Side pass test" << endl;
+ }
+
  
 }
