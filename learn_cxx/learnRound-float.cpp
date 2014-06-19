e375dbf315f83df450a1ffb1d4fb995d5b707e0b
ray <ray-debain>
2014-06-19 11:16:29 +0800

    round commet

---
+++ learn_cxx/learnRound-float.cpp
@@ -0,0 +1,17 @@
+#include <math.h>
+
+int main()
+{
+// ----------+------------->
+//    <---       <---        floor
+//     --->       --->       ceil
+//     --->      <---        trunc
+//    <--->      <--->       round
+//
+//    ?---?      ?---?       nearbyint / rint  fgetround() / fesetround()
+//
+//
+
+// http://www.cplusplus.com/reference/cmath/nearbyint/
+
+}
