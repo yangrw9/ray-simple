ce677e19437ecd5690cbaf57e669f8712afbea83
ray <ray-debain>
2014-06-20 14:24:07 +0800

    more but stop now

--- learn_cxx/learnStrongTypedef-Primary_poorman.cpp
+++ learn_cxx/learnStrongTypedef-Primary_poorman.cpp
@@ -2,11 +2,26 @@
 
 struct Turn {
   int value;
+  
+};
+
+struct Angle {
+  int value;
+  
+  // http://en.wikipedia.org/wiki/C%2B%2B11#Modification_to_the_definition_of_plain_old_data
+   // Are we still POD?  maybe standard-layout POD or trivial ?
+   
+  explicit Angle(int v) : value(v) {}  // for 13 , will no 11, 14
 };
 
 int foo(Turn v)
 {
-  printf("The value %d \n", v.value);
+  printf("The Turn value %d \n", v.value);
+}
+
+int foo(Angle v)
+{
+  printf("The Angle value %d \n", v.value);
 }
 
 int main()
@@ -17,7 +32,7 @@ int main()
   foo(t);
   
 //  foo( 13 );       // error: could not convert ‘13’ from ‘int’ to ‘Turn’
-//  foo( (Turn)13 ); // error: no matching function for call to ‘Turn::Turn(int)’
+  foo( (Turn)13 ); // error: no matching function for call to ‘Turn::Turn(int)’
 
   // g++ (Debian 4.8.2-21) 4.8.2
 
@@ -25,6 +40,8 @@ int main()
   foo( Turn{14} );   // warning: extended initializer lists only available with -std=c++11 or -std=gnu++11 [enabled by default]
   
   foo( (Turn){14} ); // What's this in C++11 stdandard?
-
+  
+  
+//  Angle a = {201};
 }
 
