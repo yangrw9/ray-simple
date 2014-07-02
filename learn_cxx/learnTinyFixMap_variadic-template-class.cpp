b8b42f427c88069cad8bdc744c688ab2b3757366
ray <ray-debain>
2014-07-02 17:14:55 +0800

    tiny fix map with static assert access

--- learn_cxx/learnTinyFixMap_variadic-template-class.cpp
+++ learn_cxx/learnTinyFixMap_variadic-template-class.cpp
@@ -5,6 +5,42 @@
 #include <initializer_list>
 #include <stdexcept>
 
+inline
+constexpr int index_of(char c, char k) {
+  return k == c ? 0 : -1;
+}
+
+
+template<typename... T>
+inline
+constexpr int index_of(char c, char k, T... others) {
+  return k == c ? sizeof...(others) : index_of(c, others...);
+}
+
+
+template<typename... T>  // must be template
+constexpr int get_index(char c, T... others)
+//template<> 
+//constexpr int index(char c, char... others)   // not accept
+{
+    //
+    //   1 2 3 4  5      // sizeof...(others) - index_of(c, others...)
+    //   3 2 1 0 -1      // index_of(c, others...)
+    //   a b c d
+    //       ^
+    //
+    // index_of(c, others...)                       Found will be          逆序号，序号从 0 开始  reversed
+    //                                              Not found will be      -1
+    //
+    // sizeof...(others) - index_of(c, others...)   Found will be          正序号，序号从 1 开始
+    //                                              Not found will be      sizeof...(others) + 1.
+    // 
+    // Following expression is used to shift 'Not Found' to -1, 'Found' is unmodified.
+    //
+   return ( sizeof...(others) - index_of(c, others...) ) % (sizeof...(others) + 1) - 1;
+}
+
+
 template <typename T, char... index>
 class TinyFixMap 
 {
@@ -14,6 +50,8 @@ class TinyFixMap
     auto it = std::find(list.begin(), list.end(), c);
     return it != list.end() ? it - list.begin() : -1;
   }
+  
+  
 public:
   T& operator[](char c) {
     int i = as_index(c);
@@ -23,6 +61,13 @@ public:
     else {
        throw std::out_of_range("Not a valid index");
     }
+  }
+  
+  template<char c>
+  T&  m() {
+    int i;
+    static_assert( get_index(c, index...) != -1, "Not a valid index "); // how to print c in message?
+    return vals[i];
   }
 };
 
@@ -37,6 +82,11 @@ int main()
   cout << "var['b'] = " << var['b'] << endl;
   cout << "var['c'] = " << var['c'] << endl;
   cout << "var['d'] = " << var['d'] << endl;
+
+  cout << "================" << endl;
+
+//	  cout << "var['a'] = " << var.m<'a'>() << endl;
+  var.m<'a'>();
 
 }
 
